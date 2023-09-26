e93caf7e36f5c83226b7233ee2535d1b311aebdb
NyatiZw <appsmaid19@gmail.com>
2023-09-26 19:35:02 +0000

    The wonders of spread syntax

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,5 @@
+// Export a function that concatenates arrays and a string
 export default function concatArrays(array1, array2, string) {
+	// Use spread syntax to concatenate the arrays and characters
 	return [...array1, ...array2, ...string.split('')];
 }
