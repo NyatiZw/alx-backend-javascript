8242d36834ad1f0b6e92dd52b4c170851d0d18b9
NyatiZw <appsmaid19@gmail.com>
2023-09-28 13:01:40 +0000

    pushing 1-make_classroom

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+	// Initialize an array of ClassRoom objects with the provided numbers
+	return (new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
