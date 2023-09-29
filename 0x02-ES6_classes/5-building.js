87e43614e470474028f111026472ae669d2c8b4f
NyatiZw <appsmaid19@gmail.com>
2023-09-29 16:18:28 +0000

    pushing abstract classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,4 @@
-export default class Building {
+class Building {
 	constructor(sqft) {
 		this._sqft = sqft;
 	}
@@ -9,12 +9,13 @@ export default class Building {
 
 
 	set sqft(newSqft) {
-		if (newSqft instanceof sqft) {
-			this._sqft = newSqft;
-		}
-	}
+		this._sqft = newSqft;
+		}	
 
 	evacuationWarningMessage() {
 		throw new Error("Class extending Building must override evacuationWarningMessage");
 	}
 }
+
+
+export default Building;
