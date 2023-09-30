b86414bf25161ba20a25dc96a66b39571c21b45d
NyatiZw <appsmaid19@gmail.com>
2023-09-30 11:02:07 +0000

    pushing Airport class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+	constructor(name, code) {
+		this._name = name;
+		this._code = code;
+	}
+
+
+	get name() {
+		return this._name;
+	}
+
+
+	set name(newName) {
+		if (typeof newName === 'string') {
+			this._name = newName;
+		}
+	}
+
+
+	get code() {
+		return this._code;
+	}
+
+
+	set code(newCode) {
+		if (typeof newCode == 'string') {
+			this._code = newCode;
+		}
+	}
+}
