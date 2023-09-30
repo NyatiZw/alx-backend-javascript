a4b6fc2d916ba87a4313f290e9b233339e201960
NyatiZw <appsmaid19@gmail.com>
2023-09-30 10:24:19 +0000

    fixing the super keyword

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,7 +2,8 @@ import Building from "./5-building.js";
 
 class SkyHighBuilding extends Building {
 	constructor(sqft, floors) {
-		super(sqft);
+		super();
+		this._sqft = sqft;
 		this._floors = floors;
 	}
 
