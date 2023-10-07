8505d48aa57c971a10502c4f025b88893cc7de95
NyatiZw <appsmaid19@gmail.com>
2023-10-07 16:12:54 +0000

    Map data strucutre

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+	const groceries = new Map();
+
+	groceries.set('Apples', 5);
+	groceries.set('Bananas', 3);
+	groceries.set('Carrots', 10);
+	groceries.set('Milk', 2);
+	groceries.set('Bread', 1);
+
+	return groceries;
+}
