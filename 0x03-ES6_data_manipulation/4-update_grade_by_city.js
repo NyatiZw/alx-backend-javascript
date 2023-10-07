2f0cc1cf25d622751a8cff2f72f16e2446c147bb
NyatiZw <appsmaid19@gmail.com>
2023-10-07 15:34:42 +0000

    combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByXity(studentList, city, newGradess) {
+	// Create copy of student list first
+	const updatedStudents = [...studentList];
+
+	// Find students in the specified city
+	const studentsInCity = updatedStudents.filter(student => student.location === city);
+
+	// Update the grades for students in the specified city
+	studentsInCity.forEach(student => {
+		const matchingGrade = newGrades.find(grade => grade.id === student.id);
+		if (matchingGrade) {
+			student.grade = matchingGrade.grade;
+		}
+	});
+
+	return updatedStudents;
+}
