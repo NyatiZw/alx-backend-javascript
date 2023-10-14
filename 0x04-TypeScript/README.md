3b238d8223c5a039361df51d49b5d7f7e13b3b27
NyatiZw <appsmaid19@gmail.com>
2023-10-14 15:11:54 +0000

    initial commit

---
+++ 0x04-TypeScript/README.md
@@ -0,0 +1,1 @@
+Task 0x04.Typescript
---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,34 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "New York",
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Smith",
+  age: 22,
+  location: "Los Angeles",
+};
+
+const studentList: Student[] = [student1, student2];
+
+//Get the table element
+const table = document.querySelector('table');
+
+// Loop through the students abd ass a row for each
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  cell1.textContent = student.firstName;
+  cell2.textContent = student.location;
+});
