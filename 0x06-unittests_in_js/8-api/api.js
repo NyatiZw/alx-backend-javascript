f68ac4267f374a21af3aca48bfa4399b8db7f974
NyatiZw <appsmaid19@gmail.com>
2023-12-12 16:44:49 +0000

    update

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.listen(port, () => {
+	console.log(`API available on http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the payment system');
+});
+
+module.exports = app;
+
