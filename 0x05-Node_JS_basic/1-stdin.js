cd89473301fa31706cae3021dd40eaee844bbba1
NyatiZw <appsmaid19@gmail.com>
2023-12-26 15:22:48 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,36 +1,13 @@
-/**
- * Function to get user input from the standard input.
- * @returns {Promise<string>} A promise that resolves to the user input.
- */
-function getUserInput() {
-	return new Promise((resolve) => {
-		const readline = require('readline');
-		const rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout,
-		});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-		rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-			rl.close();
-			resolve(answer);
-		});
-	});
-}
+process.stdin.on('readable', () => {
+	const name_input = process.stdin.read();
 
-/**
- * Main program that interacts with user through stdin and stdout
- */
-async function main() {
-	try {
-		const userName = await getUserInput();
-		console.log(`Your name is: ${userName}`);
-		console.log('This important software is now closing');
-	} catch (error) {
-		console.error('An error occurred:', error.message);
+	if (name_input) {
+		process.stdout.write(`Your name is : ${name_input}`);
 	}
-}
+});
 
-// Execute main
-
-
-main();
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
+});
