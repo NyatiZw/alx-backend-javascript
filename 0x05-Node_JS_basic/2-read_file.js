93a2cf68a5d94d5b0d9854634fb1e51f1045300a
NyatiZw <appsmaid19@gmail.com>
2023-12-26 16:23:29 +0000

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -21,7 +21,7 @@ const countStudents = (dataPath) => {
 	const dbFieldNames = fileLines[0].split(',');
 	const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-	for (const line of fieldLines.slice(1)) {
+	for (const line of fileLines.slice(1)) {
 		const studentRecord = line.split(',');
 		const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
 		const field = studentRecord[studentRecord.length - 1];
@@ -38,7 +38,7 @@ const countStudents = (dataPath) => {
 	  .reduce((pre, cur) => (pre || []).length + cur.length);
 	console.log(`Number of students: ${totalStudents}`);
 	for (const [field, group] of Object.entries(studentGroups)) {
-		const studentNames = group.map((students) => student.firstname).join(',');
+		const studentNames = group.map((student) => student.firstname).join(',');
 		console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
 	}
 };
