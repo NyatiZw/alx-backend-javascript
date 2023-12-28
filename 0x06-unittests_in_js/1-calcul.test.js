74cc4e3370838e27227e258d500ff841aa16fa1c
NyatiZw <appsmaid19@gmail.com>
2023-12-28 17:27:53 +0000

    update

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,10 +2,47 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-	it('should return the sum of rounded numbers', () => {
-		assert.strictEqual(calculateNumber(1.4, 4.5), 6);
-		assert.strictEqual(calculateNumber(1.4, 4.5), -4);
-		assert.strictEqual(calculateNumber(1.4, 4.5), 0.2);
-		assert.strictEqual(calculateNumber(1.4, 0), 'Error');
+	describe('type == "SUM"', () => {
+		it('equal positive numbers', () => {
+			assert.strictEqual(calculateNumber('SUM', 2.0, 2.0), 4);
+		});
+
+		it('equal positive numbers (alternate)', () => {
+			assert.strictEqual(calculateNumber('SUM', 2.3, 1.8), 4);
+		});
+
+		it('equal negative numbers (alternate)', () => {
+			assert.strictEqual(calculateNumber('SUM', -2.0, -2.0), -4);
+		});
+
+		it('equal negative numbers (alternate)', () => {
+			assert.strictEqual(calculateNumber('SUM', -2.0, 2.0), 0);
+		});
+
+		it('negative and positive numbers', () => {
+			assert.strictEqual(calculateNumber('SUM', -2.0, 2.0), 0);
+		});
+
+		it('positive and negative numbers', () => {
+			assert.strictEqual(calculateNumber('SUM', 2.0, -2.0), 0);
+		});
+
+		it('0 and 0', () => {
+			assert.strictEqual(calculateNumber('SUM', 0.0, 0.0), 0);
+		});
+	});
+
+	describe('type == "SUBTRACT"', () => {
+		it('equal positive numbers', () => {
+			assert.strictEqual(calculateNumber('SUBTRACT', 2.0, 2.0), 0);
+		});
+
+		it('equal positive numbers (alternate)', () => {
+			assert.strictEqual(calculateNumber('SUBTRACT', 2.3, 1.8), 0);
+		});
+
+		it('equal negative numbers', () => {
+			assert.strictEqual(calculateNumber('SUBTRACT', -2.0, -2.0);
+		});
 	});
 });
