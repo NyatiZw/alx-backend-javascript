f554bacef0815bd2e7640635575a1723bac47a73
NyatiZw <appsmaid19@gmail.com>
2023-12-28 20:52:54 +0000

    update

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,19 +1,20 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const { sendPAymentRequestToApi } = require('./4-payment');
+const sendPAymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
 	it('should call Utils.calculateNumber with correct arguments and log the correct message', () => {
-		const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-		const consoleLogSpy = sinon.spy(console, 'log');
+		const consoleLogSpy = sinon.spy(console);
+		const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
 
+		calculateNumberStub.returns(10);
 		sendPaymentRequestToApi(100, 20);
-
-		expect(calculateNumberStub.calledWithExactly('SUM', 100, 20)).to.be.true;
-		expect(consoleLogSpy.calledWithExactly('The total is: 10')).to.be.true;
-
-		calculateNumberStub.restore();
-		consoleLogSpy.restore();
+		expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+		expect(calculateNumberStub.callCount).to.be.equal(1);
+		expect(consoleLogSpy.log.calledWith('The total is: 10')).to.be.true;
+		expect(bidBrother.log.callCount).to.be.equal(1);
+		calculatwNumberStub.restore();
+		consoleLogSpy.log.restore();
 	});
 });
