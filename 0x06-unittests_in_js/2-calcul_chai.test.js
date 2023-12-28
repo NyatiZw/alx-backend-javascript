6e7c87b2496dc620903b2967a9bc3e877b9fa1a3
NyatiZw <appsmaid19@gmail.com>
2023-12-28 20:35:12 +0000

    update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,9 +3,10 @@ const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
 	it('should return the sum of rounded numbers', () => {
-		expect(calculateNumber('SUM', 1.4, 4.5), 6);
-		expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-		expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-		expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+		expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+		expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
+		expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+		expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+		expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
 	});
 });
