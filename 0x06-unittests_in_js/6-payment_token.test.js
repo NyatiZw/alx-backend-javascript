a2fbd168bbcb9249e67a6e1e574c41fbae8a0831
NyatiZw <appsmaid19@gmail.com>
2023-12-28 21:35:35 +0000

    update

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,22 +1,12 @@
 const { expect } = require('chai');
-const { getPaymentTokenFromAPI } = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-	it('should return a resolved promise with the correct data for success case', (done) => {
+	it('getPaymentTokenFromAPI(success), where sucess == true', (done) => {
 		getPaymentTokenFromAPI(true)
-		.then((result) => {
-			expect(result).to.deep.equal({ data: 'Successful response from the API' });
-			done();
-		})
-		.catch(done);
-	});
-
-	it('should return a resolved promise with undefined for failure case', (done) => {
-		getPaymentTokenFromAPI(false)
-		.then((result) => {
-			expect(result).to.be.undefined;
-			done();
-		})
-		.catch(done);
+		  .then((result) => {
+			  expect(result).to.deep.equal({ data: 'Successful response from the API' });
+			  done();
+		  });
 	});
 });
