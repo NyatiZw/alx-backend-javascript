4dc7e7a32425b862a504c0f1ebef97ac26e31a99
NyatiZw <appsmaid19@gmail.com>
2023-12-28 20:43:29 +0000

    update

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,15 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
 	it('should call Utils.calculateNumber with correct arguments', () => {
-		const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+		const calculateNumberSpy = sinon.spy(Utils,);
 
 		sendPaymentRequestToApi(100, 20);
-
-		expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
-
-
-		calculateNumberSpy.restore();
+		expect(calculateNumberSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+		expect(calculateNumberSpy.calculateNumber.callCount).to.be.equal(1);
+		calculateNumberSpy.calculateNumber.restore();
 	});
 });
