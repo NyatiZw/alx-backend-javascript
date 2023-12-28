bf709b65384ee9ec548c497e0c9d667cdbfa010b
NyatiZw <appsmaid19@gmail.com>
2023-12-28 21:00:33 +0000

    update

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,7 +4,7 @@ const sendPAymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-	it('should call Utils.calculateNumber with correct arguments and log the correct message', () => {
+	it('calls console.log with correct arguments', () => {
 		const consoleLogSpy = sinon.spy(console);
 		const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
 
@@ -13,8 +13,8 @@ describe('sendPaymentRequestToApi', () => {
 		expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
 		expect(calculateNumberStub.callCount).to.be.equal(1);
 		expect(consoleLogSpy.log.calledWith('The total is: 10')).to.be.true;
-		expect(bidBrother.log.callCount).to.be.equal(1);
-		calculatwNumberStub.restore();
+		expect(consoleLogSpy.log.callCount).to.be.equal(1);
+		calculateNumberStub.restore();
 		consoleLogSpy.log.restore();
 	});
 });
